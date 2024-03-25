5557cc557f712f6c24738dc07c6904d5d6fe7ecf
GiuliaGua <g.guarino3@studenti.unimol.it>
2024-03-25 23:51:42 +0100

    inizializzazione variabili

--- src/main/java/org/example/Hangman.java
+++ src/main/java/org/example/Hangman.java
@@ -3,11 +3,33 @@ package org.example;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Random;
+import java.util.Scanner;
+
 @Getter
 @Setter
 
 
 public class Hangman {
+    boolean game;
+    String replay;
 
+    public Hangman() {
+        // Starts game
+        game = true;
+        replay = "";
 
+        while (game == true) {
+            this.startGame();
+        }
+    }
+
+    private void startGame(){
+        replay = "";
+        String[] Wordlist = {"fun", "random", "brown", "football", "outside", "computer",
+                "basketball", "rain", "europe", "america", "hot", "food", "cool", "ice",
+                "body", "person"};
+        Random WordChoice = new Random();
+        String Word = Wordlist[WordChoice.nextInt(Wordlist.length)];
+    }
 }
