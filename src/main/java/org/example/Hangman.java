af1f6a37ac58ba28221ee98e4b52a19996938039
GiuliaGua <g.guarino3@studenti.unimol.it>
2024-03-26 00:29:10 +0100

    stampa parola nascosta con lettere indovinate

--- src/main/java/org/example/Hangman.java
+++ src/main/java/org/example/Hangman.java
@@ -31,20 +31,42 @@ public class Hangman {
                 "body", "person"};
         Random wordChoice = new Random();
         String word = wordList[wordChoice.nextInt(wordList.length)];
+        int chances = 6;
+        String hiddenWord = hiddenWords(word);
 
-        this.printStart(word);
+        this.printStart(word, hiddenWord);
     }
 
-    private void printStart(String word){
+    private void printStart(String word, String hiddenWord){
         // Welcoming and introduction
         System.out.println("Welcome to hangman.");
         String newWord = letterInWord();
+        hiddenWord = newWord;
         System.out.println("The chosen word is a length of " + word.length() + " long.");
+        System.out.println(hiddenWord);
         System.out.println("Type a letter to guess! Good luck.");
+    }
+
+    public static String hiddenWords(String word){
+        char[] temp = new char[word.length()];
+
+        for (int i = 0; i < word.length(); i++) {
+            temp[i] = word.charAt(i);
+        }
+
+        for (int i = 0; i < temp.length; i++){
+            temp[i] = '*';
+        }
+
+        String guessedLetterInWord = new String(temp);
+
+        return guessedLetterInWord;
     }
 
     public static String letterInWord(){
 
         return null;
     }
+
+
 }
