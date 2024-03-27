0515f71dc7a1167a8d50bec61ae92a6e626b0d30
GiuliaGua <g.guarino3@studenti.unimol.it>
2024-03-27 14:51:57 +0100

    Correzione errori dati da Checkstyle

--- src/main/java/org/example/Hangman.java
+++ src/main/java/org/example/Hangman.java
@@ -9,138 +9,177 @@ import java.util.Scanner;
 @Getter
 @Setter
 
-
 public class Hangman {
-    boolean game;
-    String replay;
+    /**
+     * variabile game.
+     */
+    private boolean game;
 
+    /**
+     * variabile replay.
+     */
+    private String replay;
+
+    /**
+     * variabile per numero magic checkstyle.
+     */
+    private static final int TREDUE = 32;
+
+    /**
+     * variabile magic number.
+     */
+    private static final int SEI = 6;
+
+    /**
+     * variabile chances.
+     */
+    private int chances = SEI;
+
+    /**
+     * inizio del game.
+     */
     public Hangman() {
         // Starts game
         game = true;
         replay = "";
 
-        while (game == true) {
+        while (game) {
             this.startGame();
         }
     }
 
-    private void startGame(){
+    private void startGame() {
         replay = "";
-        String[] wordList = {"fun", "random", "brown", "football", "outside", "computer",
-                "basketball", "rain", "europe", "america", "hot", "food", "cool", "ice",
-                "body", "person"};
+        String[] wordList = {"fun", "random", "brown", "football", "outside",
+                "computer", "basketball", "rain", "europe", "america", "hot",
+                "food", "cool", "ice", "body", "person"};
         Random wordChoice = new Random();
         String word = wordList[wordChoice.nextInt(wordList.length)];
-        int chances = 6;
         String hiddenWord = hiddenWords(word);
         String insertLetter = "_";
         String lettersGuessed = "";
         boolean wordDone = false;
 
-        Scanner start = new Scanner( System.in );
-        Scanner answer = new Scanner( System.in );
+        Scanner start = new Scanner(System.in);
+        Scanner answer = new Scanner(System.in);
 
         this.printStart(word,
                         hiddenWord,
                         insertLetter,
-                        chances,
                         wordDone,
                         lettersGuessed,
                         start,
                         answer);
     }
 
-    private void printStart(String word,
-                            String hiddenWord,
-                            String insertLetter,
-                            int chances,
-                            boolean wordDone,
-                            String lettersGuessed,
-                            Scanner start,
-                            Scanner answer){
+    private void printStart(final String word,
+                            final String hiddenWord,
+                            final String insertLetter,
+                            final boolean wordDone,
+                            final String lettersGuessed,
+                            final Scanner start,
+                            final Scanner answer) {
+        String tmpHiddenWord = hiddenWord;
+        String tmpInsertLetter = insertLetter;
+        boolean tmpWordDone = wordDone;
+        String tmpLettersGuessed = lettersGuessed;
 
         // Welcoming and introduction
         System.out.println("Welcome to hangman.");
-        String newWord = letterInWord(word, hiddenWord, insertLetter);
-        hiddenWord = newWord;
-        System.out.println("The chosen word is a length of " + word.length() + " long.");
-        System.out.println(hiddenWord);
+        String newWord = letterInWord(word, tmpHiddenWord, tmpInsertLetter);
+        tmpHiddenWord = newWord;
+        System.out.println("The chosen word is a length of "
+                + word.length() + " long.");
+        System.out.println(tmpHiddenWord);
         System.out.println("Type a letter to guess! Good luck.");
 
-
-        while (wordDone == false){
-            insertLetter = answer.nextLine();
+        while (!tmpWordDone) {
+            tmpInsertLetter = answer.nextLine();
             System.out.println();
 
-            if (!alreadyGuessed(lettersGuessed, insertLetter)){
-                lettersGuessed += insertLetter + ", ";
+            if (!alreadyGuessed(tmpLettersGuessed, tmpInsertLetter)) {
+                tmpLettersGuessed += tmpInsertLetter + ", ";
 
-
-                if (isLetterInWord(insertLetter, word ) == true){
-                    System.out.println("Good job! The letter " + insertLetter + " was in the word.");
-                    newWord = letterInWord(word, hiddenWord, insertLetter);
-                    hiddenWord = newWord;
-                    System.out.println("Guess another letter. You still have " + chances + " chances remaining.");
+                if (isLetterInWord(tmpInsertLetter, word)) {
+                    System.out.println("Good job! The letter "
+                            + tmpInsertLetter + " was in the word.");
+                    newWord = letterInWord(word, tmpHiddenWord,
+                            tmpInsertLetter);
+                    tmpHiddenWord = newWord;
+                    System.out.println("Guess another letter. You still have "
+                            + chances + " chances remaining.");
                     System.out.println("The letters you have guessed are: ");
-                    System.out.println(lettersGuessed.substring( 0, lettersGuessed.length()-2) + ".");
-                    System.out.println(hiddenWord);
-                    wordDone = isWordDone(word, newWord);
+                    System.out.println(tmpLettersGuessed.substring(0,
+                            tmpLettersGuessed.length() - 2) + ".");
+                    System.out.println(tmpHiddenWord);
+                    tmpWordDone = isWordDone(word, newWord);
                 } else {
-                    System.out.println("Sorry the letter " + insertLetter + " is not in the word.");
+                    System.out.println("Sorry the letter "
+                            + tmpInsertLetter + " is not in the word.");
 
                     chances = chances - 1;
 
                     // Quando il giocatore fails
-                    while (chances <= 0){
-                        System.out.println("Sorry you are out of tries. Would you like to replay(Yes/No)[Y,N]: ");
+                    while (chances <= 0) {
+                        System.out.println("Sorry you are out of tries."
+                                + " Would you like to replay[Yes,No]:");
                         replay = start.nextLine();
-                        if (replay.equalsIgnoreCase("Yes")){
-                            wordDone = true;
+                        if (replay.equalsIgnoreCase("Yes")) {
+                            tmpWordDone = true;
                             break;
-                        } else if (replay.equalsIgnoreCase("No")){
+                        } else if (replay.equalsIgnoreCase("No")) {
                             System.out.println("Goodbye.");
                             System.exit(0);
                         }
                     }
 
-                    if (wordDone == false){
-                        System.out.println("Try again. You have " + chances + " remaining.");
-                        wordDone = isWordDone(word, newWord);
-                        System.out.println("The letters you have guessed are: ");
-                        System.out.println(lettersGuessed.substring( 0, lettersGuessed.length()-2) + ".");
-                        System.out.println(hiddenWord);
+                    if (!tmpWordDone) {
+                        System.out.println("Try again. You have "
+                                + chances + " remaining.");
+                        tmpWordDone = isWordDone(word,
+                                              newWord);
+                        System.out.println("The letters you have guessed are:");
+                        System.out.println(tmpLettersGuessed.substring(0,
+                                tmpLettersGuessed.length() - 2) + ".");
+                        System.out.println(tmpHiddenWord);
                         System.out.println();
                         System.out.print("Enter another letter: ");
                     }
                 }
             } else {
-                System.out.println("Please enter a letter you haven't guessed: ");
+                System.out.println("Pls enter a letter you haven't guessed:");
                 System.out.println("The letters you have guessed are: ");
-                System.out.println(lettersGuessed.substring( 0, lettersGuessed.length()-2) + ".");
+                System.out.println(tmpLettersGuessed.substring(0,
+                        tmpLettersGuessed.length() - 2) + ".");
             }
         }
 
 
         System.out.println("\nGood Job! Would you like to play again?");
+
         replay = start.nextLine();
-        if (replay.equalsIgnoreCase("Yes") ){
+        if (replay.equalsIgnoreCase("Yes")) {
             game = true;
         }
-        if (replay.equalsIgnoreCase("No") ){
+        if (replay.equalsIgnoreCase("No")) {
             System.exit(0);
         }
 
     }
 
-    // setta la parola con gli asterischi
-    public static String hiddenWords(String word){
+    /**
+     * setta la parola con gli asterischi.
+     * @param word
+     * @return stringa
+     */
+    public static String hiddenWords(final String word) {
         char[] temp = new char[word.length()];
 
         for (int i = 0; i < word.length(); i++) {
             temp[i] = word.charAt(i);
         }
 
-        for (int i = 0; i < temp.length; i++){
+        for (int i = 0; i < temp.length; i++) {
             temp[i] = '*';
         }
 
@@ -149,32 +188,43 @@ public class Hangman {
         return asteriskWord;
     }
 
-    //place la lettera nella parola nascosta
-    public static String letterInWord(String word, String wordOld, String insertLetter){
+    /**
+     * place la lettera nella parola nascosta.
+     * @param word
+     * @param wordOld
+     * @param insertLetter
+     * @return stringa
+     */
+    public static String letterInWord(final String word,
+                                      final String wordOld,
+                                      final String insertLetter) {
         char[] temp = new char[word.length()];
 
-        for (int i = 0; i < word.length(); i++){
+        for (int i = 0; i < word.length(); i++) {
             temp[i] = word.charAt(i);
         }
 
         String insertLetterUpper = insertLetter.toUpperCase();
 
-        for (int i = 0; i<word.length(); i++){
-            if (insertLetterUpper.charAt(0) == word.charAt(i) && wordOld.charAt(i) == '*' ){
+        for (int i = 0; i < word.length(); i++) {
+            if (insertLetterUpper.charAt(0) == word.charAt(i)
+                    && wordOld.charAt(i) == '*') {
                 temp[i] = insertLetterUpper.charAt(0);
-            } else if (wordOld.charAt(i) == '*' ){
+            } else if (wordOld.charAt(i) == '*') {
                 temp[i] = '*';
             }
         }
 
-        for (int i = 0; i<word.length(); i++){
-            if (insertLetter.toLowerCase().charAt(0) == word.charAt(i) && wordOld.charAt(i) == '*' ){
-                if (insertLetter.charAt(0) >= 'a' && insertLetter.charAt(0) <= 'z' ){
-                    temp[i] = (char) (insertLetter.charAt(0) - 32);
+        for (int i = 0; i < word.length(); i++) {
+            if (insertLetter.toLowerCase().charAt(0) == word.charAt(i)
+                    && wordOld.charAt(i) == '*') {
+                if (insertLetter.charAt(0) >= 'a'
+                        && insertLetter.charAt(0) <= 'z') {
+                    temp[i] = (char) (insertLetter.charAt(0) - TREDUE);
                 } else {
                     temp[i] = (char) (insertLetter.charAt(0));
                 }
-            } else if (wordOld.charAt(i) == '*'){
+            } else if (wordOld.charAt(i) == '*') {
                 temp[i] = '*';
             }
         }
@@ -184,15 +234,21 @@ public class Hangman {
         return wordNow;
     }
 
-    // controlla se la lettera è nella parola
-    public static boolean isLetterInWord(String insertLetter, String word){
+    /**
+     * controlla se la lettera è nella parola.
+     * @param insertLetter
+     * @param word
+     * @return boolean
+     */
+    public static boolean isLetterInWord(final String insertLetter,
+                                         final String word) {
         boolean letterInWord = false;
         int i = 0;
         String currentLetter;
 
-        while (i < word.length()){
-            currentLetter = word.substring(i, i + 1 );
-            if (insertLetter.equalsIgnoreCase(currentLetter)){
+        while (i < word.length()) {
+            currentLetter = word.substring(i, i + 1);
+            if (insertLetter.equalsIgnoreCase(currentLetter)) {
                 letterInWord = true;
             }
             i = i + 1;
@@ -200,24 +256,37 @@ public class Hangman {
         return letterInWord;
     }
 
-    //controllo se la lettera che si vuole indovinare è già stata indovinata
-    public static boolean alreadyGuessed(String guessed, String letterGuessing){
+    /**
+     * controllo se la lettera che si vuole indovinare è già stata indovinata.
+     * @param guessed
+     * @param letterGuessing
+     * @return boolean
+     */
+    public static boolean alreadyGuessed(final String guessed,
+                                         final String letterGuessing) {
         char letter = letterGuessing.charAt(0);
         for (int i = 0; i < guessed.length(); i++) {
-            if (guessed.charAt(i) == letter || guessed.charAt(i) + 32 == letter){
+            if (guessed.charAt(i) == letter
+                    || guessed.charAt(i) + TREDUE == letter) {
                 return true;
             }
         }
         return false;
     }
 
-    public static boolean isWordDone(String word, String wordString){
+    /**
+     * funzione che controlla se è finita la parola.
+     * @param word
+     * @param wordString
+     * @return boolean
+     */
+    public static boolean isWordDone(final String word,
+                                     final String wordString) {
         boolean wordDone = false;
-        if (word.equalsIgnoreCase(wordString)){
+        if (word.equalsIgnoreCase(wordString)) {
             wordDone = true;
         }
         return wordDone;
     }
-
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,17 @@ package org.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
+public final class Main {
+    /**
+     * main.
+     * @param args
+     */
+    public static void main(final String[] args) {
         Hangman startHangman = new Hangman();
     }
+
+    /**
+     * costruttore per checkstyle.
+     */
+    private Main() { }
 }
